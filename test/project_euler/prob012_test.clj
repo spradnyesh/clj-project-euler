1e4275f53e6eb896bfce02e32d22505a81bfaf53
Pradnyesh Sawant <spradnyesh@gmail.com>
2014-12-08 15:08:41 +0530

    fix bug in prob012_test

--- test/project_euler/prob012_test.clj
+++ test/project_euler/prob012_test.clj
@@ -1,7 +1,7 @@
-(ns project-euler.prob001-test
+(ns project-euler.prob012-test
   (:use expectations
         project-euler.prob012))
 
 (expect 28 (triangle-number 7))
 (expect '(1 2 4 5 10 100 50 25 20) (divisors 100))
-(expect 25200 (first (main 50)))
+(expect 25200 (main 50))
