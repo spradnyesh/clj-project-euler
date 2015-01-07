a30ef667c3cbdb3fa7e74d60769a504802bf08c0
Pradnyesh Sawant <spradnyesh@gmail.com>
2015-01-07 18:29:33 +0530

    prob-27: tried 2nd approach as in https://projecteuler.net/quote_post=97-914e8, but same performance :(

--- src/project_euler/prob027.clj
+++ src/project_euler/prob027.clj
@@ -1,19 +1,34 @@
 (ns project-euler.prob027
-  (:require [project-euler.utils :as u]
-            [clojure.math.numeric-tower :as math]))
+  (:require [project-euler.utils :as u]))
 
 (defn quad [a b n]
   (if (zero? n) b
-      (+ (math/expt n 2) (* n a) b)))
+      (+ (* n n) (* n a) b)))
 
-(defn max-quad-primes [limit]
-  (apply max-key key (group-by #(nth % 2) (for [a (range (- limit) (inc limit))
-                                                b (range (- limit) (inc limit))
-                                                n (range)
-                                                :let [quad (quad a b n)]
-                                                :while (u/is-prime? quad)]
-                                            [a b n]))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn max-quad-primes-1 [limit]
+  (first (second (apply max-key key
+                        (group-by #(nth % 2)
+                                  (for [a (range (- limit) (inc limit))
+                                        b (range (- limit) (inc limit))
+                                        n (range)
+                                        :let [quad (quad a b n)]
+                                        :while (u/is-prime? quad)]
+                                    [a b n]))))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn count-primes [a b]
+  (count (for [n (range)
+               :while (u/is-prime? (quad a b n))]
+           1)))
 
-(defn main [limit]
-  (let [mqp (first (second (max-quad-primes limit)))]
+(defn max-quad-primes-2 [limit]
+  (last (sort-by #(nth % 2)
+                 (remove #(zero? (nth % 2))
+                         (for [a (range (- limit) (inc limit))
+                               b (range (- limit) (inc limit))]
+                           [a b (count-primes a b)])))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn main [limit fn]
+  (let [mqp (fn limit)]
     (* (first mqp) (second mqp))))
--- test/project_euler/prob027_test.clj
+++ test/project_euler/prob027_test.clj
@@ -3,5 +3,8 @@
         project-euler.prob027))
 
 (expect 14 (quad 1 2 3))
-(expect [5 [[-3 7 5]]] (max-quad-primes 10))
-(expect -21 (main 10))
+(expect [-3 7 5] (max-quad-primes-1 10))
+(expect 40 (count-primes 1 41))
+(expect [-3 7 6] (max-quad-primes-2 10))
+(expect -21 (main 10 max-quad-primes-1))
+(expect -21 (main 10 max-quad-primes-2))
